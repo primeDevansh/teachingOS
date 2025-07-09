64cc5e7a120b99e5565d8ded4744a6b8a9104c0c
Devansh Rastogi <devansh.rastogi@iongroup.com>
2025-07-09 12:12:48 +0530

    Remove emoji

--- devansh.c
+++ devansh.c
@@ -2,20 +2,20 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    printf("Devansh has entered your terminal 🧑‍💻\n");
+    printf("Devansh has entered your terminal!\n\n");
 
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-m") == 0 && i + 1 < argc) {
-            printf("😐 Current mood: %s\n", argv[++i]);
+            printf("Current mood: %s\n", argv[++i]);
         }
         else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
-            printf("🍕 Craving snack: %s\n", argv[++i]);
+            printf("Craving snack: %s\n", argv[++i]);
         }
         else if (strcmp(argv[i], "-h") == 0 && i + 1 < argc) {
-            printf("🧑‍🤝‍🧑 Hi %s. Are you enjoying this session?\n", argv[++i]);
+            printf("Hi %s. Are you enjoying this session?\n", argv[++i]);
         }
         else {
-            printf("❓ Unknown tag: %s\n", argv[i]);
+            printf("Unknown tag: %s\n", argv[i]);
         }
     }
 
