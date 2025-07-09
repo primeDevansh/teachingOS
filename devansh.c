b33249756b29dc75fc3a387bb0c16d54d10f64fe
Devansh Rastogi <devansh.rastogi@iongroup.com>
2025-07-09 12:07:59 +0530

    Add devansh.c

---
+++ devansh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    printf("Devansh has entered your terminal 🧑‍💻\n");
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-m") == 0 && i + 1 < argc) {
+            printf("😐 Current mood: %s\n", argv[++i]);
+        }
+        else if (strcmp(argv[i], "-s") == 0 && i + 1 < argc) {
+            printf("🍕 Craving snack: %s\n", argv[++i]);
+        }
+        else if (strcmp(argv[i], "-h") == 0 && i + 1 < argc) {
+            printf("🧑‍🤝‍🧑 Hi %s. Are you enjoying this session?\n");
+        }
+        else {
+            printf("❓ Unknown tag: %s\n", argv[i]);
+        }
+    }
+
+    return 0;
+}
